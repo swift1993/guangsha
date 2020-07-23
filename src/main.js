d2128ca053f4e1f8b2a420bad3a0998bf9ac7ff3
swift1993 <1156398454@qq.com>
2020-07-23 18:04:46 +0800

    fixed:引入axios

--- src/main.js
+++ src/main.js
@@ -9,6 +9,22 @@ import './assets/icon/iconfont.css'
 
 Vue.use(ElementUI);
 
+//引入axios
+import axios from 'axios';
+import { post, get, patch, put } from './request/http';
+
+
+// 将axios添加到原型链上
+Vue.prototype.$axios = axios;
+
+
+// 定义全局变量
+Vue.prototype.$post = post;
+Vue.prototype.$get = get;
+Vue.prototype.$patch = patch;
+Vue.prototype.$put = put;
+
+
 Vue.config.productionTip = false;
 
 new Vue({
